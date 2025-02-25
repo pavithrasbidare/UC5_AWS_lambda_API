cdc88501879866a9e17a2714efbdb808a9a11927
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-02-25 23:18:10 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -6,3 +6,10 @@ module "lamda" {
   source = "../Modules/LAMBDA"
   lambda_role_arn = module.iam.lambda_role_arn
  }
+
+module "monitoring" {
+  source              = "../Module/Monitoring"
+  function_name       = module.lambda
+  lambda_role_name    = aws_iam_role.lambda_role.name
+  log_retention_in_days = 14
+}
