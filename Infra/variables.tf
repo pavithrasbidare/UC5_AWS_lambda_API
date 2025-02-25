79c48887ffdc1f7df30c97d5a232c35890861395
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-02-25 16:58:37 +0530

    Update variables.tf

--- Infra/variables.tf
+++ Infra/variables.tf
@@ -1,11 +1,6 @@
-variable "image_url_patient" {
-  description = "The URL of the patient service Docker image in ECR"
-  type        = string
-}
-
 # In the variables.tf of the EKS module
 
-variable "image_url" {
+variable "hello_world" {
   description = "The URL of the Appointment service Docker image in ECR"
   type        = string
 
