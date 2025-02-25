4ffc3600478ca50738d62a3062e9580ad9654b84
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-02-25 14:23:29 +0530

    Create main.tf

---
+++ Modules/LAMBDA/main.tf
@@ -0,0 +1,54 @@
+resource "aws_lambda_function" "lamda_hello_world" {
+  function_name = "hello_world"
+  role          = var.lambda_role_arn
+  package_type  = "Image"
+  image_uri     = var.image_name
+
+  memory_size = 512
+  timeout     = 30
+
+  environment {
+    variables = {
+      ENV = var.environment
+    }
+  }
+}
+
+resource "aws_apigatewayv2_api" "lambda_api" {
+  name          = "lambda-container-api"
+  protocol_type = "HTTP"
+  
+  cors_configuration {
+    allow_origins = ["*"]
+    allow_methods = ["GET", "POST", "PUT", "DELETE"]
+    allow_headers = ["Content-Type", "Authorization"]
+    max_age       = 300
+  }
+}
+
+resource "aws_apigatewayv2_stage" "lambda_stage" {
+  api_id = aws_apigatewayv2_api.lambda_api.id
+  name   = "prod"
+  auto_deploy = true
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id = aws_apigatewayv2_api.lambda_api.id
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+  integration_uri    = aws_lambda_function.my_lambda.invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "lambda_route" {
+  api_id = aws_apigatewayv2_api.lambda_api.id
+  route_key = "ANY /{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.my_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn = "${aws_apigatewayv2_api.lambda_api.execution_arn}/*/*"
+}
