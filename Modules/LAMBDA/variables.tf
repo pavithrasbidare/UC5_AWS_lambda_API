8100c5cfb4e4f8d7dfb34ef80d70275163318dca
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-02-25 21:24:45 +0530

    Update variables.tf

--- Modules/LAMBDA/variables.tf
+++ Modules/LAMBDA/variables.tf
@@ -1,7 +1,7 @@
 variable "region" {
   description = "AWS region where resources will be deployed"
   type        = string
-  default     = "us-east-1"
+  default     = "us-west-2"
 }
 
 variable "image_name"{
