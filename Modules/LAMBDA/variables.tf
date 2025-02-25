1d59fd41b1413fbfa4b2d256876fc0ca93e56272
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-02-25 20:55:25 +0530

    Update variables.tf

--- Modules/LAMBDA/variables.tf
+++ Modules/LAMBDA/variables.tf
@@ -5,7 +5,7 @@ variable "region" {
 }
 
 variable "image_name"{
-default="302263075199.dkr.ecr.us-east-1.amazonaws.com/hello_world:latest"
+default="302263075199.dkr.ecr.us-east-1.amazonaws.com/hello_world@sha256:aa99b156ae7f7b18ad28a32960496776498330f5c74844f65b9d18a87aa2b3ae"
 }
 
 variable "lambda_role_arn" {
