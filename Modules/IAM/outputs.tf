8741a90538bd5fafb5393cca2d03cc8baf1986c3
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-02-25 17:37:26 +0530

    Update outputs.tf

--- Modules/IAM/outputs.tf
+++ Modules/IAM/outputs.tf
@@ -1,4 +1,7 @@
 output "lambda_role_arn" {
-  description = "The ARN of the IAM role for Lambda"
-  value       = aws_iam_role.lambda_role.arn
+  value = aws_iam_role.lambda_exec_role.arn
+}
+
+output "attach_basic_execution" {
+    value = aws_iam_role_policy_attachment.lambda_exec_policy_attachment
 }
