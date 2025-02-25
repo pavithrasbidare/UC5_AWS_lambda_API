ff4dea2c9970e7db7776415631982050ecf86bfc
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-02-25 23:26:48 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -8,8 +8,11 @@ module "lamda" {
  }
 
 module "monitoring" {
-  source              = "../Module/Monitoring"
-  function_name       = module.lambda
-  lambda_role_name    = aws_iam_role.lambda_role.name
+  source              = "../Modules/Monitoring"
+  lambda_function_name = module.lambda_function.lambda_function_name
+  lambda_function_arn  = module.lambda_function.lambda_function_arn
+  api_gateway_url      = module.lambda_function.api_gateway_url
+  api_gateway_stage    = module.lambda_function.api_gateway_stage
+  lambda_role_name     = module.iam.lambda_role_name
   log_retention_in_days = 14
 }
