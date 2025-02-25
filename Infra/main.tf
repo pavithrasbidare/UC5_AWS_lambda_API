df6a38c70196810935d098b364dee2886fa960d3
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-02-25 23:33:33 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -9,10 +9,10 @@ module "lamda" {
 
 module "monitoring" {
   source              = "../Modules/Monitoring"
-  lambda_function_name = module.lambda_function.lambda_function_name
-  lambda_function_arn  = module.lambda_function.lambda_function_arn
-  api_gateway_url      = module.lambda_function.api_gateway_url
-  api_gateway_stage    = module.lambda_function.api_gateway_stage
+  lambda_function_name = module.lambda.lambda_function_name
+  lambda_function_arn  = module.lambda.lambda_function_arn
+  api_gateway_url      = module.lambda.api_gateway_url
+  api_gateway_stage    = module.lambda.api_gateway_stage
   lambda_role_name     = module.iam.lambda_role_name
   log_retention_in_days = 14
 }
