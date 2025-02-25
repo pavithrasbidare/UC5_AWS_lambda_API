3e82328c7b8d55ed20358c5a31b148d4e38f6327
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-02-25 21:22:34 +0530

    Update provider.tf

--- Infra/provider.tf
+++ Infra/provider.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "us-west-2"
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.3.0"
+}
+
