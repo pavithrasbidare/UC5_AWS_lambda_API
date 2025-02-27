a93fc21095081b0d159e5ffd3ff900982cfb46f5
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-02-27 17:59:46 +0530

    Update index.js

--- App/hello_world/index.js
+++ App/hello_world/index.js
@@ -1,4 +1,14 @@
 exports.handler = async (event) => {
+    const path = event.path;
+
+    if (path !== "/expected-path") {
+        const response = {
+            statusCode: 404,
+            body: JSON.stringify("Not Found"),
+        };
+        return response;
+    }
+
     const response = {
         statusCode: 200,
         body: JSON.stringify("Hello, World!"),
